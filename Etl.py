36934904ffa8be1ded7026a349e4daa674a89a15
jajfasfaa <manuelabreu1212@protonmail.com>
2026-06-12 07:54:28 -0500

    c

--- Etl.py
+++ Etl.py
@@ -1,7 +1,18 @@
+import io
+import os
+import uuid
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient, ContainerClient, BlobBlock, BlobClient, StandardBlobTier
 import kagglehub
 from kagglehub import KaggleDatasetAdapter
 import pandas as pd
+
+
 DATASET = "kaisersafdf/messy-e-ccomerce-dataset"
+account_url = "https://<jtaawstorage>.blob.core.windows.net"
+credential = DefaultAzureCredential()
+blob_service_client = BlobServiceClient(account_url=account_url,credential=credential)
+container_name = "mycontainer"
 
 def extract_data() -> dict[str, pd.DataFrame]:
     print(f"Downloading dataset: {DATASET}")
@@ -13,6 +24,32 @@ def extract_data() -> dict[str, pd.DataFrame]:
     print(f"  Loaded: {df.shape[0]:,} rows, {df.shape[1]} columns")
     return {"data": df}
 
+def load_to_blob(tables:dict[str,pd.DataFrame]):
+        """Uploads each DataFrame as a JSON blob to Azure Blob Storage."""
+        account_url = "https://jtaawstorage.blob.core.windows.net"
+        credential = DefaultAzureCredential()
+        blob_service_client = BlobServiceClient(account_url=account_url,credential=credential)
+        container_name = "mycontainer"
+
+        
+        
+        for table_name, df in tables.items():
+             blob_name = f"raw/{table_name}.json"
+             json_string = df.to_json(orient="records", indent=2, force_ascii=False)
+             blob_client  = blob_service_client.get_blob_client(
+                container= container_name ,blob=blob_name)
+        
+             blob_client .upload_blob(json_string, overwrite=True)
+             print(f"  Uploaded: {blob_name}")
+             
+
+   
+
+
+
 if __name__ == "__main__":
     tables = extract_data()
-    print(tables["data"].head())
+    load_to_blob(tables)
+
+    """tables = extract_data()
+    print(tables["data"].head())"""
