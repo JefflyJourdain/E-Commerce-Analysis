f4a8f1b5d8dd9362016f09200e395a34ab03c83e
jajfasfaa <manuelabreu1212@protonmail.com>
2026-06-19 12:23:19 -0500

    eda

--- etl.sql
+++ etl.sql
@@ -43,6 +43,21 @@ FactSales AS (
             )AS delivery_date,
             customer_id,
             sales_channel,
+            quantity,
+            unit_price,
+            unit_cogs,
+            discount_pct,
+            gross_revenue,
+            net_revenue,
+            cogs_total,
+            gross_profit,
+            gross_margin_pct,
+            shipping_cost,
+            
+
+
+
+
             geography_key
 
 
@@ -86,21 +101,58 @@ unique_products as (
             ELSE NULL
         END ) AS subcategory,
         REPLACE(category,'Storage & Organization','Storage') AS category ,
+        reorder_point,supplier_id,
         ROW_NUMBER() over(partition by product_id order by product_id) as num_fila 
 
     from ecommerce_raw),
+    
 
 
 DimProduct AS (
-    SELECT  product_id, product_name,subcategory, category
+    SELECT  product_id, product_name,subcategory, category,reorder_point,supplier_id
     from unique_products
-    WHERE num_fila = 1)
+    WHERE num_fila = 1),
+
+
+FactInventory AS (
+    SELECT top 10 product_id, inventory_on_hand, 
+        days_in_stock, supplier_lead_time_days,warehouse_id,fulfillment_status
+        from ecommerce_raw),
+
+returns_processed AS (
+    SELECT order_id,
+        COALESCE(
+            TRY_CAST(TRY_CONVERT(DATETIME,return_date,101) AS DATE),
+                TRY_CAST(TRY_CONVERT(DATETIME,return_date,103) AS DATE),
+                TRY_CAST(TRY_CONVERT(DATETIME,return_date,105) AS DATE),
+                TRY_CAST(TRY_CONVERT(DATETIME,return_date,120) AS DATE)
+            )AS return_date,
+            
+        return_reason,refund_amount,
+        CASE 
+            WHEN return_flag IS NULL THEN 0
+            WHEN lower(return_flag) IN ('y','true','yes') THEN 1
+            WHEN lower(return_flag) IN ('n','false','no') THEN 0
+            ELSE return_flag
+            END as return_flag
+            
+    from ecommerce_raw
+),
+FactReturns AS (
+    select  order_id,return_date,return_reason,sum(refund_amount) AS refund_amount 
+
+
+        FROM returns_processed
+        WHERE return_flag !=0
+        GROUP BY order_id,return_date,return_reason
+        )
+
+
+SELECT top 10 *
+from FactReturns;
 
 
 
 
 
 
-
-SELECT top 20 * 
-FROM DimCustomer;
